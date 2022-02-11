8e9971f581ee15970cdb628049d666ddf4fd188c
Noah Gardner <ngngardner@gmail.com>
2022-02-11 16:04:12 -0500

    proposal

--- tex-env.nix
+++ tex-env.nix
@@ -1,10 +1,11 @@
 # Generated with tex2nix 0.0.0
-{ texlive, extraTexPackages ? {} }:
+{ texlive, extraTexPackages ? { } }:
 (texlive.combine ({
-    inherit (texlive) scheme-small;
-    "xcolor" = texlive."xcolor";
-    "times" = texlive."times";
-    "placeins" = texlive."placeins";
-    "microtype" = texlive."microtype";
+  inherit (texlive) scheme-small;
+  "xcolor" = texlive."xcolor";
+  "times" = texlive."times";
+  "placeins" = texlive."placeins";
+  "microtype" = texlive."microtype";
+  "preprint" = texlive."preprint";
 
 } // extraTexPackages))
